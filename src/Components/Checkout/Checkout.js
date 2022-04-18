d33ca1304ce32d1081b9daa693a31af2a9ff6d09
ador <rashedulhaque111@gmail.com>
2022-04-19 02:23:49 +0600

    checkout page done

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Checkout = () => {
+    const { serviceId } = useParams();
     return (
         <div>
-            <h1>helo</h1>
+            <h1 className='text-3xl'>Welcome to Details: {serviceId}</h1>
+
+            <button value="button" className="px-4 py-2 rounded bg-green-400 text-white hover:bg-orange-600 my-4 w-50" id="whoobe-ibemp">Checkout</button>
         </div>
     );
 };
